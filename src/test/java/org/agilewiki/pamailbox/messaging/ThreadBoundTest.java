5c7eee62ea0f64a7347744bf74f39f11ad2e1172
Bill La Forge <laforge49@gmail.com>
2013-03-29 10:29:53 +0530

    restored test1, added ThreadBoundTest

--- src/test/java/org/agilewiki/pamailbox/messaging/ThreadBoundTest.java
+++ src/test/java/org/agilewiki/pamailbox/messaging/ThreadBoundTest.java
@@ -1,21 +1,27 @@
 package org.agilewiki.pamailbox.messaging;
 
 import junit.framework.TestCase;
-
 import org.agilewiki.pactor.Mailbox;
 import org.agilewiki.pactor.MailboxFactory;
+import org.agilewiki.pactor.ResponseProcessor;
 import org.agilewiki.pamailbox.DefaultMailboxFactoryImpl;
 
 /**
  * Test code.
  */
-public class Test1 extends TestCase {
+public class ThreadBoundTest extends TestCase {
     public void testa() throws Exception {
         final MailboxFactory mailboxFactory = new DefaultMailboxFactoryImpl();
+        final Mailbox boundMailbox = mailboxFactory.createThreadBoundMailbox();
         final Mailbox mailbox = mailboxFactory.createMailbox();
         final Actor1 actor1 = new Actor1(mailbox);
-        final String result = actor1.hi1.call();
-        assertEquals("Hello world!", result);
+        actor1.hi1.send(boundMailbox, new ResponseProcessor<String>() {
+            @Override
+            public void processResponse(String response) throws Exception {
+                assertEquals("Hello world!", response);
+            }
+        });
+        boundMailbox.processMessages();
         mailboxFactory.close();
     }
 }
