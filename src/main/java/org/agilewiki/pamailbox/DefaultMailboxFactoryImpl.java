127e7465215dfc8d0e6cf2f2a93d0536753d76f1
dallaybatta <vickykak@gmail.com>
2013-04-02 22:33:29 +0530

    Removed unused ExecutorService related imports.

--- src/main/java/org/agilewiki/pamailbox/DefaultMailboxFactoryImpl.java
+++ src/main/java/org/agilewiki/pamailbox/DefaultMailboxFactoryImpl.java
@@ -3,8 +3,6 @@ package org.agilewiki.pamailbox;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.agilewiki.pactor.Mailbox;
