df98a46d3e83736b2a89b72b06e219e0e7e6528f
Bill La Forge <laforge49@gmail.com>
2013-04-06 10:07:13 +0530

    Added @override to PAMailboxFactory.createMailbox methods.

--- src/main/java/org/agilewiki/pamailbox/PAMailboxFactory.java
+++ src/main/java/org/agilewiki/pamailbox/PAMailboxFactory.java
@@ -4,12 +4,26 @@ import org.agilewiki.pactor.MailboxFactory;
 
 public interface PAMailboxFactory extends MailboxFactory {
     void submit(final Runnable task) throws Exception;
+
+    @Override
     PAMailbox createMailbox();
+
+    @Override
     PAMailbox createMailbox(final boolean mayBlock);
+
+    @Override
     PAMailbox createMailbox(final int initialBufferSize);
+
+    @Override
     PAMailbox createMailbox(final boolean mayBlock, final int initialBufferSize);
+
+    @Override
     PAMailbox createMailbox(final boolean mayBlock, final Runnable onIdle);
+
+    @Override
     PAMailbox createMailbox(final boolean mayBlock, final int initialBufferSize,
                           final Runnable onIdle);
+
+    @Override
     PAMailbox createThreadBoundMailbox(final Runnable _messageProcessor);
 }
